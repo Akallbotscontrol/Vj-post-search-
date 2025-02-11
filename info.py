e98fbad95cbfe39efb34b970b64ba99fb352a2ec
Ak all bots Control <akallbotscontrol@gmail.com>
2025-02-11 21:10:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -7,10 +7,10 @@
 import os
 from os import environ
 
-API_ID       = int(environ.get("API_ID", ""))
-API_HASH     = environ.get("API_HASH", "")
+API_ID       = int(environ.get("API_ID", "22289037"))
+API_HASH     = environ.get("API_HASH", "ced5a994db436661f9af63c4b5247ac6")
 BOT_TOKEN    = environ.get("BOT_TOKEN", "7490778826:AAHQocs5ICH4SsMdBfvbS1eAB52FnEajCbM")
 DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://RMCFILTERBOT:RMCFILTERBOT@rmcfilterbot.bm5g1.mongodb.net/?retryWrites=true&w=majority&appName=RMCFILTERBOT")
 LOG_CHANNEL  = int(environ.get("LOG_CHANNEL", "-1002442107508"))
-ADMIN        = int(environ.get("ADMIN", ""))
+ADMIN        = int(environ.get("ADMIN", "7037505654"))
 CHANNEL      = environ.get("CHANNEL", "@VJ_Botz")
